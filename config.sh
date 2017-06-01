466491e7566883352f305deeedc42482b0a8da03
Zach Deibert <zachdeibert@gmail.com>
2017-05-31 19:39:31 -0500

    Fixed password length

--- config.sh
+++ config.sh
@@ -3,8 +3,11 @@ set -e
 
 echo -n "SSID: "
 read ssid
-echo -n "PSK:  "
-read psk
+psk=
+while [ $(echo -n "$psk" | wc -c) -lt 8 ] || [ $(echo -n "$psk" | wc -c) -gt 63 ]; do
+    echo -n "PSK:  "
+    read psk
+done
 
 rm -f .config.sh
 cat <<EOF > .config.sh
