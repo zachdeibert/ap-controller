edc6216df3e34723f92d509b36c5c292abe92ff6
Zach Deibert <zachdeibert@gmail.com>
2017-05-31 19:49:39 -0500

    Added DNS configuration

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-. "$(dirname "$0")/.config.sh"
+cd "$(dirname "$0")"
+
+. .config.sh
 
 sudo cp nginx.conf /etc/nginx/sites-enabled/default
 sudo mkdir -p /usr/share/ap-controller/controller/
@@ -12,3 +14,5 @@ sudo cp dhcpcd.conf /etc/dhcpcd.conf
 sed -e "s|{{ *ssid *}}|$ssid|g" -e "s|{{ *psk *}}|$psk|g" hostapd.conf | sudo tee /etc/hostapd/hostapd.conf > /dev/null
 sudo cp hostapd.sh /etc/default/hostapd
 sudo sed -e "s|^DAEMON_CONF=$|DAEMON_CONF=/etc/hostapd/hostapd.conf|g" -i /etc/init.d/hostapd
+sudo cp bind9.conf /etc/bind/named.conf.default-zones
+sudo cp db.root /etc/bind/db.root
