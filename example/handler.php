d5e50a490886fa8854a695271e98497ae53201e2
Zach Deibert <zachdeibert@gmail.com>
2017-05-31 21:44:57 -0500

    Added some more documentation

--- example/handler.php
+++ example/handler.php
@@ -2,7 +2,24 @@
     /*
      * By the time control enters this portion of the code, there is a variable
      * named `$message` that contains the message the controller sent.  This
-     * script should not output anything ever.
+     * script should not output anything ever.  Here are some functions that are
+     * already defined for you:
+     *
+     * Log::write($message) - Writes a message to the application log
+     *                        (/var/log/ap-controller.log)
+     *
+     * new Gpio($direction, $number) - Creates a new GPIO pin controller on pin
+     *                                 `$number` with the specified
+     *                                 `$direction`.  The `$direction` may be
+     *                                 either `Direction::In` or
+     *                                 `Direction::Out`.  The `$number` of each
+     *                                 pin can be found at:
+     * https://www.element14.com/community/docs/DOC-73950/l/raspberry-pi-2-model-b-gpio-40-pin-block-pinout
+     *
+     * $gpio->setValue($value) - Sets the output voltage of a pin (0 = 0V, 255 =
+     *                           3.3V)
+     *
+     * $gpio->getValue() - Gets the input voltage of a pin (0 = 0V, 255 = 3.3V)
      */
     $gpio = new Gpio(Direction::Out, 21);
     if ($message == "on") {
